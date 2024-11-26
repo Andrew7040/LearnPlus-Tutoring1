a0478e2bbac6a805be60809c6739dff64585c23c
Andrew7040 <andrew.mccleod@lander.edu>
2024-11-26 18:43:28 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD9V6dYyCVBnq2v_rrdb_UXVD6H3ZMiXPo",
+    authDomain: "website-4350d.firebaseapp.com",
+    projectId: "website-4350d",
+    storageBucket: "website-4350d.firebasestorage.app",
+    messagingSenderId: "60469440464",
+    appId: "1:60469440464:web:b48b852ebfbe914055d3d5",
+    measurementId: "G-N212HRSKNP"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+
+const submit = document.getElementById('submit');
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Sign in user
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // User signed in
+            const user = userCredential.user;
+            alert("Login successful!");
+            window.location.href = "index.html"; 
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            alert(errorMessage); 
+        });
+});
